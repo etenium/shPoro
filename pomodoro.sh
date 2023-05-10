52fc6cb3312c2a22aaf141c46dc7862b40bf0815
etenie <99820286+etenium@users.noreply.github.com>
2023-05-10 16:08:09 +0000

    Fix the long breaks and show some more info

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# ⌄⌄⌄Make sure to set path to the file here⌄⌄⌄
+#Set the sound file location here
 SOUND_FILE_PATH="/path/to/file.mp3"
 
 RED='\033[0;31m'
@@ -10,16 +10,14 @@ GREEN='\033[0;32m'
 BOLD='\033[1m'
 RESET='\033[0m'
 
-clear
+echo -en "\e]2;shPomo\007"
 echo -en "${WHITE}${BOLD}Length of the pomodoro: ${RESET}"
 read countdown_minutes
-clear
 echo -en "${WHITE}${BOLD}Length of the break: ${RESET}"
 read break_minutes
-clear
+original_break_minutes=$break_minutes
 echo -en "${WHITE}${BOLD}Length of the longer break: ${RESET}"
 read longer_break_minutes
-clear
 echo -en "${WHITE}${BOLD}Countdowns before the longer break: ${RESET}"
 read countdowns_before_longer_break
 clear
@@ -27,72 +25,71 @@ countdowns_completed=0
 total_time=0
 
 while true; do
-    echo -e "${RED}${BOLD}Counting down ${RESET}${WHITE}${BOLD}$countdown_minutes ${RESET}${RED}${BOLD}minutes...${RESET}"
+	echo -e "${RED}${BOLD}Counting down ${RESET}${WHITE}${BOLD}$countdown_minutes ${RESET}${RED}${BOLD}minutes...${RESET}"
 
-    countdown_end_time=$(date -d "+$countdown_minutes minutes" +%s)
+	countdown_end_time=$(date -d "+$countdown_minutes minutes" +%s)
 
-    while [ $(date +%s) -lt "$countdown_end_time" ]; do
-        countdown_remaining_time=$(date -u -d @$((countdown_end_time - $(date +%s))) +%M:%S)
-        printf "\r${RED}${BOLD}Count:${RESET} ${WHITE}${BOLD}%s${RESET}" "$countdown_remaining_time"
-        sleep 1
-    done
+	while [ $(date +%s) -lt "$countdown_end_time" ]; do
+		countdown_remaining_time=$(date -u -d @$((countdown_end_time - $(date +%s))) +%M:%S)
+		printf "\r${RED}${BOLD}Count:${RESET} ${WHITE}${BOLD}%s${RESET}" "$countdown_remaining_time"
+		sleep 1
+	done
 
-    printf "\n"
+	printf "\n"
 
-    countdowns_completed=$((countdowns_completed + 1))
+	countdowns_completed=$((countdowns_completed + 1))
 
-    total_time=$((total_time + countdown_minutes))
+	total_time=$((total_time + countdown_minutes))
 
-    notify-send "Countdown complete!" "Countdown duration: $countdown_minutes minutes"
+	notify-send "Countdown complete!" "Countdown duration: $countdown_minutes minutes"
 
-    echo -e "${WHITE}${BOLD}Total time elapsed: $total_time minutes${RESET}"
+	mpg123 -q $SOUND_FILE_PATH &
 
-    mpg123 -q $SOUND_FILE_PATH &
+	if [ "$countdowns_before_longer_break" -gt 0 ] && [ $((countdowns_completed > 0 && countdowns_completed % countdowns_before_longer_break == 0)) -eq 1 ]; then
+		if [ "$longer_break_minutes" -eq 0 ]; then
+			break_minutes=$original_break_minutes
+		else
+			break_minutes=$longer_break_minutes
+		fi
+	else
+		break_minutes=$original_break_minutes
+	fi
+	echo -en "${WHITE}${BOLD}Do you want to start a $break_minutes minute break? (Y/n) ${RESET}"
+	read -n 1 start_break
+	clear
+	if [ "$start_break" = "" ] || [ "$start_break" = "y" ] || [ "$start_break" = "Y" ]; then
+		echo -e "${WHITE}${BOLD}Total time elapsed: $total_time minutes${RESET}"
+		echo -e "${GREEN}${BOLD}Countdowns completed: ${RESET}${WHITE}${BOLD}$countdowns_completed${RESET}"
+		echo -e "${RED}${BOLD}Starting ${RESET}${WHITE}${BOLD}${break_minutes} ${RESET}${RED}${BOLD}minute break...${RESET}"
 
-    if [ "$countdowns_before_longer_break" -gt 0 ] && [ $((countdowns_completed % countdowns_before_longer_break)) -eq 0 ]; then
-        if [ "$longer_break_minutes" -eq 0 ]; then
-            break_minutes=$break_minutes
-        else
-            break_minutes=$longer_break_minutes
-        fi
-    fi
+		break_end_time=$(date -d "+$break_minutes minutes" +%s)
 
-    echo -en "${WHITE}${BOLD}Do you want to start a $break_minutes minute break? (Y/n) ${RESET}"
-    read -n 1 start_break
-    clear
-    if [ "$start_break" = "" ] || [ "$start_break" = "y" ] || [ "$start_break" = "Y" ]; then
-        echo -e "${RED}${BOLD}Starting ${RESET}${WHITE}${BOLD}${break_minutes} ${RESET}${RED}${BOLD}minute break...${RESET}"
+		while [ $(date +%s) -lt "$break_end_time" ]; do
+			break_remaining_time=$(date -u -d @$((break_end_time - $(date +%s))) +%M:%S)
+			printf "\r${RED}${BOLD}Break:${RESET} ${WHITE}${BOLD}%s${RESET}" "$break_remaining_time"
+			sleep 1
+		done
 
-        break_end_time=$(date -d "+$break_minutes minutes" +%s)
+		printf "\n"
 
-        while [ $(date +%s) -lt "$break_end_time" ]; do
-            break_remaining_time=$(date -u -d @$((break_end_time - $(date +%s))) +%M:%S)
-            printf "\r${RED}${BOLD}Break:${RESET} ${WHITE}${BOLD}%s${RESET}" "$break_remaining_time"
-            sleep 1
-        done
+		notify-send "Break complete!" "Break duration: $break_minutes minutes"
 
-        printf "\n"
+		mpg123 -q $SOUND_FILE_PATH &
 
-        notify-send "Break complete!" "Break duration: $break_minutes minutes"
+		echo -en "${WHITE}${BOLD}Do you want to start a $countdown_minutes minute pomodoro? (Y/n) ${RESET}"
+		read -n 1 restart
+		clear
+		if [ "$restart" = "" ] || [ "$restart" = "y" ] || [ "$restart" = "Y" ]; then
+			echo -e "${WHITE}${BOLD}Total time elapsed: $total_time minutes${RESET}"
+		else
+			break
+		fi
 
-        mpg123 -q $SOUND_FILE_PATH &
-
-        echo -en "${WHITE}${BOLD}Do you want to continue the pomodoro? (Y/n) ${RESET}"
-        read -n 1 restart
-        clear
-        if [ "$restart" = "" ] || [ "$restart" = "y" ] || [ "$restart" = "Y" ]; then
-            echo -e "${BLUE}${BOLD}Restarting countdown and break...${RESET}"
-        else
-            break
-        fi
-
-        echo -e "${GREEN}${BOLD}Countdowns completed: ${RESET}${WHITE}${BOLD}$countdowns_completed${RESET}"
-    else
-        break
-    fi
+		echo -e "${GREEN}${BOLD}Countdowns completed: ${RESET}${WHITE}${BOLD}$countdowns_completed${RESET}"
+	else
+		break
+	fi
 done
-
 echo -e "${RED}${BOLD}Exiting countdown and break timer.${RESET}"
 echo -e "${GREEN}${BOLD}Countdowns completed: ${RESET}${WHITE}${BOLD}$countdowns_completed${RESET}"
 echo -e "${WHITE}${BOLD}Total time elapsed: $total_time minutes${RESET}"
-
